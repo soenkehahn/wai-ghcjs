64689f892a60c643389d6f41bd3aa500f3b7c709
Sönke Hahn <soenkehahn@gmail.com>
2015-09-30 01:30:25 +0800

    better code for error output

--- src/Network/Wai/Shake/Ghcjs.hs
+++ src/Network/Wai/Shake/Ghcjs.hs
@@ -1,4 +1,5 @@
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE QuasiQuotes #-}
 
 module Network.Wai.Shake.Ghcjs where
 
@@ -6,6 +7,8 @@ import           Control.Monad
 import qualified Data.ByteString.Lazy as LBS
 import           Data.Default
 import           Data.String.Conversions
+import           Data.String.Interpolate
+import           Data.String.Interpolate.Util
 import           Language.ECMAScript3.PrettyPrint
 import           Language.ECMAScript3.Syntax
 import           Language.ECMAScript3.Syntax.CodeGen
@@ -19,23 +22,43 @@ import           System.Process
 
 serveGhcjs :: FilePath -> [FilePath] -> FilePath -> Application
 serveGhcjs mainFile sourceDirs buildDir request respond = respond =<< do
+  createDirectoryIfMissing True buildDir
+  outDir <- ghcjsOrErrorToConsole mainFile sourceDirs buildDir
+  case pathInfo request of
+    [] -> serveFile "text/html" (outDir </> "index" <.> "html")
+    [file] | ".js" == takeExtension (cs file) -> do
+      fileExists <- doesFileExist (outDir </> cs file)
+      if fileExists
+        then serveFile "application/javascript" (outDir </> cs file)
+        else return $ responseLBS notFound404 [] "404 - Not Found"
+
+ghcjsOrErrorToConsole :: FilePath -> [FilePath] -> FilePath -> IO FilePath
+ghcjsOrErrorToConsole mainFile sourceDirs buildDir = do
   let outPattern = buildDir </> takeBaseName mainFile
       outDir = outPattern <.> "jsexe"
       sourceDirFlags = unwords $ map ("-i" ++) sourceDirs
-  createDirectoryIfMissing True buildDir
-  initializeFiles outPattern
   (exitCode, out, err) <- readProcessWithExitCode "ghcjs"
     ["-O0", mainFile, "-o", outPattern, sourceDirFlags, "-outputdir=" ++ buildDir </> "output"]
     ""
-  case pathInfo request of
-    [] -> serveFile "text/html" (outDir </> "index" <.> "html")
-    [file] | ".js" == takeExtension (cs file) ->
-      case exitCode of
-        ExitSuccess -> do
-          serveFile "application/javascript" (outDir </> cs file)
-        ExitFailure _ -> do
-          return $ responseLBS ok200 [] (createJsToConsole (out ++ "\n" ++ err))
-    _ -> return $ responseLBS notFound404 [] "404 - Not Found"
+  case exitCode of
+    ExitSuccess -> do
+      return ()
+    ExitFailure _ -> do
+      createDirectoryIfMissing True outDir
+      let jsCode = createJsToConsole (out ++ "\n" ++ err)
+      LBS.writeFile (outDir </> "runmain.js") jsCode
+      LBS.writeFile (outDir </> "all.js") jsCode
+      writeFile (outDir </> "index.html") $ unindent [i|
+        <!DOCTYPE html>
+        <html>
+          <head>
+          </head>
+          <body>
+          </body>
+          <script language="javascript" src="runmain.js" defer></script>
+        </html>
+      |]
+  return outDir
 
 createJsToConsole :: String -> LBS.ByteString
 createJsToConsole msg =
@@ -49,12 +72,3 @@ serveFile :: String -> FilePath -> IO Response
 serveFile contentType file = do
   c <- LBS.readFile file
   return $ responseLBS ok200 [("Content-Type", cs contentType <> "; charset=utf-8")] c
-
-initializeFiles :: String -> IO ()
-initializeFiles outPattern = do
-  indexExists <- doesFileExist (outPattern <.> "jsexe" </> "index.html")
-  when (not indexExists) $ do
-    withSystemTempDirectory "serve-ghcjs" $ \ dir -> do
-      let mainFile = dir </> "Main.hs"
-      writeFile mainFile "main = return ()"
-      callCommand (unwords ["ghcjs", "-O0", mainFile, "-o", outPattern])
